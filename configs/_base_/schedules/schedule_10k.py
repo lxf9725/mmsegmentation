91435aaae89b288de10237a805c6696748b39d95
master <912742049@qq.com>
2023-04-24 22:28:04 +0800

    新增256x256数据集

--- configs/_base_/schedules/schedule_10k.py
+++ configs/_base_/schedules/schedule_10k.py
@@ -8,17 +8,17 @@ param_scheduler = [
         eta_min=1e-4,
         power=0.9,
         begin=0,
-        end=40000,
+        end=10000,
         by_epoch=False)
 ]
-# training schedule for 40k
-train_cfg = dict(type='IterBasedTrainLoop', max_iters=40000, val_interval=4000)
+# training schedule for 10k
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=10000, val_interval=1000)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
     timer=dict(type='IterTimerHook'),
     logger=dict(type='LoggerHook', interval=100, log_metric_by_epoch=False),
     param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=4000),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=1000),
     sampler_seed=dict(type='DistSamplerSeedHook'),
     visualization=dict(type='SegVisualizationHook'))
