f3452ff0c5ce5ead94fc298572a8f8477892e938
lxf9725 <912742049@qq.com>
2023-05-04 22:21:29 +0800

    修改setr_vaihingen配置文件

--- configs/swin/swin-base-patch4-window7-resnet-in1k-pre_upernet_1xb4-160k_potsdam-512x512.py
+++ configs/swin/swin-base-patch4-window7-resnet-in1k-pre_upernet_1xb4-160k_potsdam-512x512.py
@@ -4,11 +4,43 @@ _base_ = [
 ]
 crop_size = (512, 512)
 data_preprocessor = dict(size=crop_size)
-res_checkpoint_file = ''
-swin_checkpoint_file = 'pretrain/swin_base_patch4_window7_224.pth'
 model = dict(
+    data_preprocessor=data_preprocessor,
     backbone=dict(
-        embed_dims=128,
-        depths=[2, 2, 18, 2],
-        num_heads=[4, 8, 16, 32]),
-    decode_head=dict(in_channels=1024, num_classes=6))
+        swin_embed_dims=128,
+        swin_depths=[2, 2, 18, 2],
+        swin_num_heads=[4, 8, 16, 32]),
+    decode_head=dict(in_channels=[128, 256, 512, 1024], num_classes=6),
+    auxiliary_head=dict(in_channels=512, num_classes=6)
+)
+# AdamW optimizer, no weight decay for position embedding & layer norm
+# in backbone
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=1500,
+        end=160000,
+        by_epoch=False,
+    )
+]
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+train_dataloader = dict(batch_size=4)
+val_dataloader = dict(batch_size=1)
+test_dataloader = val_dataloader
